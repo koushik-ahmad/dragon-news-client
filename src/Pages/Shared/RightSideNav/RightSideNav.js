ef63584e099da794a15e26eee744782acccd0197
koushik-ahmad <koushikahmad123@gmail.com>
2023-06-20 22:22:22 +0600

    first commit

---
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { FaGithub, FaGoogle, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch } from "react-icons/fa";
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <ButtonGroup vertical>
+                <Button className='mb-2' variant="outline-primary"><FaGoogle></FaGoogle> Login with Google</Button>
+                <Button variant="outline-dark"><FaGithub></FaGithub> Login with Github</Button>
+            </ButtonGroup>
+            <div className='mt-4'>
+                <h5>Find us on</h5>
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp></FaWhatsapp> WhatsApp</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitch></FaTwitch> Twitch</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'>Vestibulum at eros</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <div>
+                <BrandCarousel></BrandCarousel>
+            </div>
+        </div>
+    );
+};
+
+export default RightSideNav;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCbjE-9z6qzQGrn3iaKbVuHWWRR7GqFgz4",
+    authDomain: "dragon-news-41466.firebaseapp.com",
+    projectId: "dragon-news-41466",
+    storageBucket: "dragon-news-41466.appspot.com",
+    messagingSenderId: "180950733760",
+    appId: "1:180950733760:web:95214c08abca4149a2ae25"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
